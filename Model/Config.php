0e8d7c7c82ab039d4c19aca85c8e01231046908f
anna-stawarz <anna.stawarz@int.pl>
2021-09-27 23:54:27 +0200

    Add option to check if module is enabled

--- Model/Config.php
+++ Model/Config.php
@@ -4,7 +4,25 @@ declare(strict_types=1);
 
 namespace Infiright\SampleRequest\Model;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
 class Config
 {
+    private const XML_PATH_SAMPLE_REQUEST_ACTIVE = 'sample_request_base_config/general/active';
 
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function isModuleActive(string $scopeType = ScopeConfigInterface::SCOPE_TYPE_DEFAULT): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::XML_PATH_SAMPLE_REQUEST_ACTIVE, $scopeType);
+    }
 }
